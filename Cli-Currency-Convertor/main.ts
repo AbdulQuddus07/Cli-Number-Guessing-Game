1a6e0766b3ab04508aa5f05c48a1ad52a0aa227a
Abdul Quddus <abdulquddus33kk@gmail.com>
2024-04-08 00:50:31 +0500

    "cli-currency-converter"

--- Cli-Currency-Convertor/main.ts
+++ Cli-Currency-Convertor/main.ts
@@ -1,19 +1,52 @@
 #! /usr/bin/env node
-import inuirer from "inquirer"
+import inuqirer from "inquirer";
+import chalk, { Chalk } from "chalk";
 
+console.log(chalk.blue.bold.italic("\n\tWEllCOME TO CODE WITH- 'ABDUL-QUDDUS' -CURRENCY CONVERTER\n"));
 
 // Define the list of currencies and thier exchange rates:
-const currency:any = {
-    "USB":1,
-    "EUR":0.9,
-    "GBP":113,
-    "INR":1.3,
-    "AFG":69,
-    "PKR":280
+const currency: any = {
+    "USB": 1, // Base Currency
+    "EUR": 0.9, // European Currency
+    "JAP": 113, // Japanese Curreny
+    "CAD": 1.3, // Canadian Curreny
+    "INR": 71, // Indian Curreny
+    "AFG": 69, // Afghanistan Curreny
+    "PKR": 280 // PAkistan Rupees
+    // Add more currencies and their exchange rates here
 }
-let user_answer = await inuirer.prompt([{
-    name: "from",
-    type: "list",
-    message: "Enter from Currency",
-    choices: []
-}])
+let user_answer = await inuqirer.prompt([
+    {
+        name: "from_currency",
+        type: "list",
+        message: chalk.yellow("Select the currency to convert from:"),
+        choices: ["USB", "EUR", "JAP", "CAD", "INR", "AFG", "PKR",]
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: chalk.yellow("Select the currency to convert into:"),
+        choices: ["USB", "EUR", "JAP", "CAD", "INR", "AFG", "PKR",]
+    },
+    {
+
+        name: "amount",
+        type: "input",
+        message: chalk.grey("Enter the amount to convert:",)
+
+
+    }
+]);
+
+// Perform currency coversion by using formula
+let from_amount = currency[user_answer.from_currency];
+let to_amount = currency[user_answer.to_currency];
+let amount = user_answer.amount
+
+// Formula of conversion
+let base_amount = amount / from_amount
+let converted_amount = base_amount * to_amount
+
+// Display the converted amount
+console.log(`Converted Amount = ${chalk.green(converted_amount.toFixed(2))}`);
+
